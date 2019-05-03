b51599d3b1aa0eff6e54038bfb968ef6255c64f4
ceramics.atx <openstudio.atx@gmail.com>
2019-05-03 12:15:16 -0400

    Added description

--- app/src/main/java/co/hannalupi/kivasample/model/Loan.kt
+++ app/src/main/java/co/hannalupi/kivasample/model/Loan.kt
@@ -2,4 +2,8 @@ package co.hannalupi.kivasample.model
 
 import com.google.gson.annotations.SerializedName
 
-data class Loan(val id: Int, val name: String, @SerializedName("loan_amount") val loanAmount: Int, val status: String)
+data class Loan(val id: Int, val name: String, @SerializedName("loan_amount") val loanAmount: Int, val status: String, val description: Map<String, Any>) {
+
+        val descriptionText: String?
+                get() = (description["texts"] as Map<String, String>)["en"]
+}
